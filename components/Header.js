079ae460d2ce0ff7e4415ec1e6fc34002bc91f2f
rajatb555 <rajat.bopalkar@gmail.com>
2021-08-31 14:17:24 +0530

    Landing page

---
+++ components/Header.js
@@ -0,0 +1,26 @@
+import { UserCircleIcon } from '@heroicons/react/solid'
+
+function Header() {
+    return (
+        <header className=" sticky top-0 z-50 grid
+        grid-cols-2 bg-white shadow-md w-max p-2 md:px-10">
+            {/* Left */}
+            <div className="relative flex items-center h-10 mr-0 md:mr-96
+            cursor-pointer my-auto hover:scale-105 transform transition duration-150 ease-out">
+                <a className="text-xl text-blue-600 font-semibold">Task</a>
+                <a className="text-xl text-gray-800 font-semibold">Force</a>
+            </div>
+
+            {/* Right */}
+            <div className="flex space-x-4 items-center justify-end text-gray-500">
+                <button className="hidden md:inline border-2 p-2 rounded-md shadow-sm hover:shadow-lg hover:scale-105 transform 
+                transition duration-150 ease-out">Join as a creator</button>
+                <div className="flex items-center space-x-2 border-2 p-2 rounded-full hover:border-blue-400">
+                    <UserCircleIcon className="h-6 cursor-pointer" />
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header
---
+++ components/Main.js
@@ -0,0 +1,31 @@
+import { ArrowNarrowRightIcon } from '@heroicons/react/solid'
+
+function Main() {
+    return (
+        <div className="mt-40 mb-40">
+          <div className="flex flex-1 border-2 p-2 rounded-md max-w-sm mt-10 cursor-pointer bg-white
+          hover:border-blue-400 shadow-sm hover:shadow-lg hover:scale-105 transform transition duration-150 ease-out">
+            <p className="font-semibold mr-10 text-xl">Creator</p>
+            <div>
+                <ArrowNarrowRightIcon className="mt-2 h-6"/>
+            </div>
+            <div>
+                <p className="ml-2 text-gray-500">Create your own community. Assign tasks, review submissions and reward your community members.</p>
+            </div>
+          </div>
+
+          <div className="flex flex-1 border-2 p-2 rounded-md max-w-sm mt-10 cursor-pointer bg-white
+          hover:border-blue-400 shadow-sm hover:shadow-lg hover:scale-105 transform transition duration-150 ease-out">
+            <p className="font-semibold mr-10 text-xl">Member</p>
+            <div>
+                <ArrowNarrowRightIcon className="mt-2 h-6"/>
+            </div>
+            <div>
+                <p className="ml-2 text-gray-500">Be a part of community. Contribute and get rewarded by the creator.</p>
+            </div>
+          </div>
+        </div>
+    )
+}
+
+export default Main
